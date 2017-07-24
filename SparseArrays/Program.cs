208527f6a374feff7fba878af7da583bbe11e419
Anton <anton.abyzov@gmail.com>
2017-07-25 00:53:12 +0300

    sparse arrays resolved

--- SparseArrays/Program.cs
+++ SparseArrays/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SparseArrays
 {
@@ -6,7 +8,23 @@ namespace SparseArrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var n = Convert.ToInt32(Console.ReadLine());
+            var strings = new List<string>();
+            var queries = new List<string>();
+            while (n-- > 0)
+            {
+                strings.Add(Console.ReadLine());
+            }
+            var q = Convert.ToInt32(Console.ReadLine());
+            while (q-- > 0)
+            {
+                queries.Add(Console.ReadLine());
+            }
+
+            foreach (var query in queries)
+            {
+                Console.WriteLine(strings.Count(x => x == query));
+            }
         }
     }
 }
